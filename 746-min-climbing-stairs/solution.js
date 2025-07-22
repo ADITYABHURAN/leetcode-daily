8011350f7345b522a030eb911355a341f1abe3f1
Aditya Bhuran <adityabhuran240802@gmail.com>
2025-07-22 08:31:00 +0530

    memoization and tabulation approach

--- 746-min-climbing-stairs/solution.js
+++ 746-min-climbing-stairs/solution.js
@@ -12,3 +12,44 @@ var minCostClimbingStairs = function(cost) {
    }
     return Math.min(helper(0),helper(1));
 };
+
+
+//memoization approach - time complexity O(n) and space complexity O(n)
+//Runtime 2 ms Beats 52.67%
+var minCostClimbingStairs = function(cost) {
+    let n = cost.length;
+    let memo = Array(n).fill(-1);
+    
+    function helper(index) {
+      //base cases
+      if(index>=n) return n;
+      if(memo[index]!==-1) return memo[index];
+      let one = cost[index] + helper(index + 1);
+       let two = cost[index] + helper(index + 2);
+       memo[index] = Math.min(one, two)
+       return memo[index];
+
+    }
+
+    return Math.min(helper(0), helper(1));
+ };
+
+//tabulation Approach 
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minCostClimbingStairs = function(cost) {
+  let n = cost.length;
+  let dp = Array(n+1).fill(0);
+
+  dp[0]=0;
+  dp[1]=0;
+
+  for(let i = 2;i<=n;i++) {
+    let costTocomeFromOnestepback = cost[i-1] + dp[i-1];
+    let costTocomeFromTwostepback = cost[i-2] + dp[i-2];
+    dp[i] = Math.min(costTocomeFromOnestepback, costTocomeFromTwostepback);
+  }
+  return dp[n];
+};
