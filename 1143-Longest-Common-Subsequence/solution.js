62672268afb2f0d50122db20694ba1a597a75800
Aditya Bhuran <adityabhuran240802@gmail.com>
2025-08-25 08:35:58 -0400

    memoization

--- 1143-Longest-Common-Subsequence/solution.js
+++ 1143-Longest-Common-Subsequence/solution.js
@@ -22,4 +22,36 @@ var longestCommonSubsequence = function(text1, text2) {
      return helper(0,0);
 };
 
+//memoization step 
+//1 create a dp array
+//2 check if the value is already computed in the base case check if there is -1 if not return the same value 
+//3 before returning the value store it in the dp array
+
+var longestCommonSubsequence = function(text1, text2) {
+     const n = text1.length;
+     const m = text2.length;
+     const dp = Array.from({length:n},()=>Array(m).fill(-1));
+
+     function helper(index1,index2){
+        //return the lcs 
+
+        //base case 
+        if(index1>=n || index2>=m) {
+            return 0;
+        }
+        if(dp[index1][index2]!==-1) {
+        return dp[index1][index2];
+            //abc
+            //abdef
+        }
+        //recurssive case 
+        if (text1[index1]===text2[index2]) {
+            dp[index1][index2] = 1+helper(index1+1,index2+1);
+        } else {
+        dp[index1][index2] = Math.max(helper(index1+1,index2),helper(index1,index2+1));
+        }
+        return dp[index1][index2];
+     }
+     return helper(0,0);
+};
 
