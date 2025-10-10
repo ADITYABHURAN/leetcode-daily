1d923c80fe91956ec62507d2abdf0e14374acb53
Aditya Bhuran <adityabhuran240802@gmail.com>
2025-10-10 09:00:57 -0400

    Implement memoization approach for lengthOfLIS function to optimize performance

--- 300-Longest-Increasing-Subsequence/solution.js
+++ 300-Longest-Increasing-Subsequence/solution.js
@@ -21,3 +21,40 @@ var lengthOfLIS = function(nums) {
     }
     return helper (0,-1)
 };
+
+//time complexity O(2^n)
+//space complexity O(n) for the recursion stack
+
+//Method 2 : memoization approach
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function (nums) {
+    let n = nums.length
+    //dp array to store the value of the subproblems
+    let dp = Array.from({length:n},()=>Array(n+1).fill(-1));
+
+    function helper(curr, prev) {
+        //base case 
+        if (curr >= n) {
+            return 0;
+        }
+        //check if the value is already computed , if yes return it , we filled all the boxes with -1 
+        if(dp[curr][prev+1]!==-1){
+            return dp[curr][prev+1];
+        }
+        //recurssive case 
+        let exclude = helper(curr + 1, prev);
+        let include = 0;
+        if (prev === -1 || nums[curr] > nums[prev]) {
+            include = 1 + helper(curr + 1, curr)
+        }
+        //store the value in the dp array before returning it
+        dp[curr][prev+1] = Math.max(include, exclude);
+        return dp[curr][prev+1];
+    //*Note ; prev+1 is used because prev can be -1 and we cannot access negative index in array
+    }
+    return helper(0, -1)
+};
