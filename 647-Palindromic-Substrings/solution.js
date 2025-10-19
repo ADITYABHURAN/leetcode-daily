ba3acf6da8c0f12d58be8d699b6f2c3d7ca2c7b4
Aditya Bhuran <adityabhuran240802@gmail.com>
2025-10-19 02:33:16 -0400

    Implement tabulation method for countSubstrings to improve efficiency in counting palindromic substrings

--- 647-Palindromic-Substrings/solution.js
+++ 647-Palindromic-Substrings/solution.js
@@ -43,3 +43,35 @@ var countSubstrings = function(s) {
     return res;
 };
 
+
+
+//tabulation method
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countSubstrings = function(s) {
+    let n = s.length;
+    let dp = Array.from(Array(n),()=>Array(n).fill(false));
+    let res = 0;
+    // l is the length of the substring
+    for(let l=1;l<=n;l++){
+        // i is the starting index of the substring
+        for(let i=0;i<=n-l;i++){
+            // j is the ending index of the substring
+            let j = i + l -1;
+            if(i===j){
+                dp[i][j] = true;
+                res +=1;
+                // if the length of the substring is 2 and both characters are same
+            }else if(s[i]===s[j] && (j===1+i || dp[i+1][j-1])){
+            dp[i][j] = true;
+            res +=1;
+            }
+
+        }
+    }
+    return res;
+    
+
+};
