74dac6586b949f11c1600de7be12494beacda3b3
Aditya Bhuran <adityabhuran240802@gmail.com>
2025-11-23 14:35:03 -0500

    Implement reverseString function to reverse a character array in-place

---
+++ 344. Reverse String/solution.js
@@ -0,0 +1,17 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    let i = 0;
+    let j = s.length - 1;
+
+    while (i < j) {
+        // we temporarily store the value at index i so that we don't lose it when we overwrite it with s[j]
+        const temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+        i++;
+        j--;
+    }
+};
