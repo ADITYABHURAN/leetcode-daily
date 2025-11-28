f594fbddcea64370d1cd503cee05ff71fdeb3bfc
Aditya Bhuran <adityabhuran240802@gmail.com>
2025-11-27 22:04:07 -0500

    Implement twoSum function to find indices of two numbers that add up to the target

---
+++ 167-Two-Sum-II-Input-Array-Is-Sorted/solution.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    let i = 0;
+    let j = numbers.length - 1;
+
+    while (i < j) {
+    const sum = numbers[i] + numbers[j];
+
+    if (sum > target) {
+    j = j - 1;
+    } else if ( sum < target) {
+    i = i + 1;
+    } else {
+        return [i + 1, j + 1];
+    }
+
+
+    }
+};
