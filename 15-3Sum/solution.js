71eaf68a49926a61a166140059307ca0f86d66cc
Aditya Bhuran <adityabhuran240802@gmail.com>
2025-11-27 22:23:07 -0500

    Implement threeSum function to find all unique triplets that sum to zero

---
+++ 15-3Sum/solution.js
@@ -0,0 +1,38 @@
+var threeSum = function(nums) {
+    nums.sort((a, b) => a - b);    // Step 1: sort
+    const res = [];
+
+    for (let i = 0; i < nums.length - 2; i++) {
+
+        // Skip duplicate values for nums[i]
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+        let left = i + 1;
+        let right = nums.length - 1;
+
+        while (left < right) {
+            const sum = nums[i] + nums[left] + nums[right];
+
+            if (sum === 0) {
+                res.push([nums[i], nums[left], nums[right]]);
+
+                // Skip duplicates on left
+                while (left < right && nums[left] === nums[left + 1]) left++;
+
+                // Skip duplicates on right
+                while (left < right && nums[right] === nums[right - 1]) right--;
+
+                left++;
+                right--;
+            }
+            else if (sum < 0) {
+                left++;
+            }
+            else {
+                right--;
+            }
+        }
+    }
+
+    return res;
+};
