2a5af13e4339df16faf73013b21db71f6aa0b941
Aditya Bhuran <adityabhuran240802@gmail.com>
2025-12-24 00:00:15 -0500

    Add implementation of searchInsert function to find the insert position of a target in a sorted array

---
+++ 35. Search Insert Position/solution.js
@@ -0,0 +1,19 @@
+var searchInsert = function(nums, target) {
+    let low = 0;
+    let high = nums.length - 1;
+
+    while (low <= high) {
+        const mid = Math.floor((low + high) / 2);
+
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+
+    // low is the correct insert position
+    return low;
+};
