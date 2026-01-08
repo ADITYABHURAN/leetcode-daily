9bd04a1f2f0c58517181cbb1926093fbf778ce49
Aditya Bhuran <adityabhuran240802@gmail.com>
2026-01-08 08:10:39 -0500

    Refactor pairSum function to use a two-pointer approach and add reversell function for reversing the linked list

--- 2130. Maximum Twin Sum of a Linked List/solution.js
+++ 2130. Maximum Twin Sum of a Linked List/solution.js
@@ -31,3 +31,59 @@ var pairSum = function(head) {
     }
     return max;
 };
+
+
+
+
+
+
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number}
+ */
+ var reversell =  function(node) {
+        let curr = node;
+        let prev = null;
+
+        while(curr !== null) {
+        let next = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = next;
+        }
+        return prev;
+    };
+
+var pairSum = function(head) {
+   let slow = head;
+   let fast = head;
+
+   while(fast !== null && fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
+   }
+   
+   //reverse 2nd half
+   let p2 = reversell(slow);
+   let p1  = head;
+   let max = -Infinity;
+
+   while(p1 !== null && p2 !== null) {
+    max = Math.max(max, p1.val + p2.val)
+
+   p1 = p1.next;
+   p2 = p2.next;
+
+   }
+
+  return max;
+};
