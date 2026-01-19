988c7026fbca892549c129f257ab720714773da3
Aditya Bhuran <adityabhuran240802@gmail.com>
2026-01-19 13:08:00 -0500

    Add implementation of minSubArrayLen function to find the minimum size of a subarray with a sum greater than or equal to the target

---
+++ 209. Minimum Size Subarray Sum/solution.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} target
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minSubArrayLen = function(target, nums) {
+    let size = Number.MAX_VALUE;
+    let sum = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        sum = 0;
+         for (let j = i; j < nums.length; j++) {
+            sum = sum + nums[j];
+
+            if (sum >= target) {
+                size = Math.min(size, j - i + 1);
+                break;
+            }
+         }
+    }
+    return size === Number.MAX_VALUE ? 0 : size;
+};
