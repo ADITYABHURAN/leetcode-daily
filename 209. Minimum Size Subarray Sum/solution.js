c7d21e12fbf3b45b03769704c4a1be8a98fd5248
Aditya Bhuran <adityabhuran240802@gmail.com>
2026-01-19 22:25:39 -0500

    Add optimal implementation of minSubArrayLen function using a sliding window approach

--- 209. Minimum Size Subarray Sum/solution.js
+++ 209. Minimum Size Subarray Sum/solution.js
@@ -21,3 +21,33 @@ var minSubArrayLen = function(target, nums) {
     }
     return size === Infinity ? 0 : size;
 };
+
+
+
+
+//optimal solution
+/**
+ * @param {number} target
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minSubArrayLen = function(target, nums) {
+    let size = Infinity;
+    let sum = 0;
+
+    let i = 0;
+    let j = 0;
+
+    while (j < nums.length) {
+        //expand window
+        sum = sum + nums[j];
+
+        while (sum >= target) {
+            size = Math.min(size, j - i + 1);
+            sum = sum - nums[i];
+            i = i + 1;
+        }
+     j = j + 1;
+    }
+    return size === Infinity ? 0 : size;
+};
