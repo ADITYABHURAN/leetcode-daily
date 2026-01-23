889da4e8a2c661a00a841d5fa7222f6710e3d586
Aditya Bhuran <adityabhuran240802@gmail.com>
2026-01-23 09:00:27 -0500

    Add implementation of containsNearbyDuplicate function to check for nearby duplicates within a given range

---
+++ 219. Contains Duplicate II/solution.js
@@ -0,0 +1,24 @@
+var containsNearbyDuplicate = function(nums, k) {
+    const set = new Set();
+
+    //first window 
+    // i < Math.min(k, nums.length) because if 
+    // nums = [1,2,3,4,2] and if k  = 100
+    // then k will go till 1-99 and then malfunction and this is not good fpor time complexity so it will choose the smallest from k and nums.length  
+    for (let i = 0; i < Math.min(k, nums.length); i++) {
+    if(set.has(nums[i])) {
+        return true;
+    }
+    set.add(nums[i]);
+    }
+
+    for (let i = k; i < nums.length; i++) {
+    if(set.has(nums[i])) {
+        return true;
+    }
+    set.add(nums[i]);
+    set.delete(nums[i - k]);
+    }
+    
+    return false;
+};
