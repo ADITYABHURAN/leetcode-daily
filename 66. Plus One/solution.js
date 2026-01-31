dcdd5ed40266daf87840373ea95368c7ac665706
Aditya Bhuran <adityabhuran240802@gmail.com>
2026-01-30 23:56:26 -0500

    Add implementation of plusOne function to increment a number represented as an array of digits

---
+++ 66. Plus One/solution.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    for (let i = digits.length - 1; i >= 0; i--) {
+        if (digits[i] < 9) {
+            digits[i]++;
+            return digits;
+        }
+        digits[i] = 0;
+    }
+    // All digits were 9, so we need a new leading 1
+    return [1, ...digits];
+};
