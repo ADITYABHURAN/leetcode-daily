8fb9597150d163aeaaad119038356a52640d5bee
Aditya Bhuran <adityabhuran240802@gmail.com>
2026-02-09 18:13:04 -0500

    Add optimized solution for totalFruit using a sliding window approach

--- 904. Fruit Into Baskets/solution.js
+++ 904. Fruit Into Baskets/solution.js
@@ -28,3 +28,35 @@ var totalFruit = function(fruits) {
 }
 return max;
 };
+
+
+
+
+
+//optimized approach
+var totalFruit = function(fruits) {
+    let max = 0;
+    const map = new Map();
+    let i = 0;
+    let j = 0;
+
+    while (j < fruits.length) {
+        // Add fruit at position j (not i!)
+        map.set(fruits[j], (map.get(fruits[j]) || 0) + 1);
+
+        // Shrink window if more than 2 types
+        while (map.size > 2) {  // size is a property, not a method
+            map.set(fruits[i], map.get(fruits[i]) - 1);
+            
+            if (map.get(fruits[i]) === 0) {
+                map.delete(fruits[i]);
+            }
+            i++;
+        }
+        
+        max = Math.max(max, j - i + 1);
+        j++;  // Don't forget to increment j!
+    }
+    
+    return max;
+};
